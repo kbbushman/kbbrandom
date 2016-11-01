d39b7eee842810f4ccb336c7e7438704b7baf4b2
kbbushman <k.bushman@yahoo.com>
2016-10-31 17:49:05 -0700

    initial commit of gem boilerplate

---
+++ lib/kbbrandom.rb
@@ -0,0 +1,13 @@
+require "kbbrandom/version"
+
+module Kbbrandom
+  def random_string(length, include_uppercase = true, include_lowercase = true, include_numbers = false)
+    o = []
+    o.push ('a'..'z') if include_uppercase
+    o.push ('A'..'Z') if include_lowercase
+    
+    o.push (0..9) if include_numbers
+    o = o.map { |i| i.to_a }.flatten
+    string = (0...length).map { o[rand(o.length)] }.join
+  end
+end
