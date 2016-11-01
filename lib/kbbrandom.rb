7bf5e838084ec7f8d2ddf218e1605403727e2919
kbbushman <k.bushman@yahoo.com>
2016-10-31 23:06:28 -0700

    fixes class/module definitions

--- lib/kbbrandom.rb
+++ lib/kbbrandom.rb
@@ -1,13 +1,15 @@
 require "kbbrandom/version"
 
-class Kbbrandom
-  def self.random_string(length, include_uppercase = true, include_lowercase = true, include_numbers = false)
-    o = []
-    o.push ('a'..'z') if include_uppercase
-    o.push ('A'..'Z') if include_lowercase
-    
-    o.push (0..9) if include_numbers
-    o = o.map { |i| i.to_a }.flatten
-    string = (0...length).map { o[rand(o.length)] }.join
-  end
+module Kbbrandom
+	class MakeA
+	  def self.random_string(length, include_uppercase = true, include_lowercase = true, include_numbers = false)
+	    o = []
+	    o.push ('a'..'z') if include_uppercase
+	    o.push ('A'..'Z') if include_lowercase
+	    
+	    o.push (0..9) if include_numbers
+	    o = o.map { |i| i.to_a }.flatten
+	    string = (0...length).map { o[rand(o.length)] }.join
+	  end
+	end
 end
